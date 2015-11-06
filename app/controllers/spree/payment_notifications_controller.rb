e4e7649f71b8066b5bf7df9f49624401b49f0fff
dbwinger <dbwinger@gmail.com>
2015-11-06 10:34:31 -0500

    change back to how it was

--- app/controllers/spree/payment_notifications_controller.rb
+++ app/controllers/spree/payment_notifications_controller.rb
@@ -37,7 +37,7 @@ module Spree
         order.payments << payment
         payment.started_processing
         
-        payment.complete
+        order.payment.complete
         logger.info("PayPal_Website_Standard: order #{order.number} (#{order.id}) -- completed payment")
 
         until @order.state == "complete"
